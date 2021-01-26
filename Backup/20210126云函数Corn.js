56c57f4b3e46b66b139f32e75703ccb68806c379
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-01-26 15:52:22 +0800

    Update and rename 20210119corn.js to 20210126云函数Corn.js

---
+++ Backup/20210126云函数Corn.js
@@ -0,0 +1,19 @@
+Collect
+0 15 0-23/2 * * * *
+jd_pet&jd_joy&jd_joy_feedPets&jd_joy_steal&jd_plantBean&jd_moneyTree&jd_bookshop&jd_fruit&jd_jdfactory&jd_nianCollect
+	
+hour8-12-16-20
+0 6 8,12,16,20 * * * *
+jd_speed&jd_superMarket&jd_crazy_joy_coin
+
+check-1
+0 10 0 * * * *
+JD_DailyBonus&jx_sign&jd_club_lottery&jd_bean_home&jd_necklace&jd_syj&jd_nh&jd_immortal
+
+check-2
+0 20 0 * * * *
+jd_Redpocket&jd_cash&jd_kd&jd_car&jd_rankingList&jd_jdzz&jd_small_home&jd_crazy_joy&jd_nian
+
+check-3
+0 20 0 * * * *
+jd_Redpocket&jd_cash&jd_kd&jd_car&jd_rankingList&jd_jdzz&jd_small_home&jd_crazy_joy&jd_nian&jd_nian_sign&jd_nian-ar
