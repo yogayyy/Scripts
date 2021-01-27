ae649e4fbc799696cf4327a14a7d41f96f987c7c
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-01-27 12:46:44 +0800

    Update 20210126云函数Corn.js

--- Backup/20210126云函数Corn.js
+++ Backup/20210126云函数Corn.js
@@ -1,19 +1,28 @@
 Collect
 0 15 0-23/2 * * * *
 jd_pet&jd_joy&jd_joy_feedPets&jd_joy_steal&jd_plantBean&jd_moneyTree&jd_bookshop&jd_fruit&jd_jdfactory&jd_nianCollect
-	
-hour8-12-16-20
-0 6 8,12,16,20 * * * *
+京东萌宠，宠汪汪，喂养，偷狗粮，种豆得豆，摇钱树，口袋读书，京东农场，京东工厂，炸年兽鞭炮收集
+
+every_4_hours
+0 6 */4 * * * *
 jd_speed&jd_superMarket&jd_crazy_joy_coin
+京东加速，京东超市，疯狂JOY挂机
 
 check-1
 0 10 0 * * * *
-JD_DailyBonus&jx_sign&jd_club_lottery&jd_bean_home&jd_necklace&jd_syj&jd_nh&jd_immortal
+jd_bean_sign&jx_sign&jd_club_lottery&jd_bean_home&jd_necklace&jd_syj&jd_shop
+京东签到，京喜签到，摇京豆，领京豆额外奖励，点点券，十元街，京豆年货
 
 check-2
 0 20 0 * * * *
-jd_Redpocket&jd_cash&jd_kd&jd_car&jd_rankingList&jd_jdzz&jd_small_home&jd_crazy_joy&jd_nian
+jd_Redpocket&jd_cash&jd_kd&jd_car&jd_rankingList&jd_jdzz&jd_small_home&jd_crazy_joy&jd_live
+全民开红包，签到领现金，快递签到，汽车签到，排行榜，赚赚，东东小窝，疯狂JOY任务
 
-check-3
-0 20 0 * * * *
-jd_Redpocket&jd_cash&jd_kd&jd_car&jd_rankingList&jd_jdzz&jd_small_home&jd_crazy_joy&jd_nian&jd_nian_sign&jd_nian-ar
+review（复查）也可手机执行
+0 0 21 * * * *
+jd_crazy_joy&jd_cash&jd_sgmh&jd_live
+
+temporary（临时活动，到期请及时删除）
+0 30 0,20 * * * *
+jd_nian&jd_nian_sign&jd_nian-ar&jd_nh&jd_immortal
+京东炸年兽，炸年兽签到，炸年兽AR，京东年货，京东永乐书院
