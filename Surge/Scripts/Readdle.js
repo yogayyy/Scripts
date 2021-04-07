3e5bf225e4f2c105306ba5d4e240e26c52b1a507
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-04-07 15:45:47 +0800

    Create Readdle.js

---
+++ Surge/Scripts/Readdle.js
@@ -0,0 +1,18 @@
+re('\\w{1,2}\/\\w{1,2}\/\\w{4}@"subscriptionState":"\\w+"@"is(.+)User":false','25/11/2099@"subscriptionState":"active"@"is$1User":true');
+
+function re() {
+var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
