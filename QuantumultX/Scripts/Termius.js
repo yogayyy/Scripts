d5f38ebea6d33021f9531888969f8e8a396dcacf
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-04-07 15:56:03 +0800

    Update Termius.js

--- QuantumultX/Scripts/Termius.js
+++ QuantumultX/Scripts/Termius.js
@@ -2,13 +2,12 @@
 Termius unlocks local pro v 
 (From the author @Maasea)
 
-Surge4:
-http-response https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Termius.js
-
+Surge:
+http-response https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ requires-body=1,max-size=0,script-path=Termius.js
 QX：
-https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Termius.js
+https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-bodyTermius.js
 
-Surge & QX MITM = api.termius.com
+MITM = api.termius.com
 */
 
 let obj=JSON.parse($response.body)
@@ -17,4 +16,3 @@ obj.account["plan_type"] = "Premium";
 obj.account["user_type"] = "Premium";
 obj.account["current_period"]["until"] = "2099-10-10T03:27:34";
 $done({body:JSON.stringify(obj)})
-//bynubyta
