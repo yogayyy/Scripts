7baf84054db9670b7a5eac52429461a5b2263c27
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-09-15 19:45:54 +0800

    Update jd_half_redrain.js

--- QuantumultX/Scripts/JD/jd_half_redrain.js
+++ QuantumultX/Scripts/JD/jd_half_redrain.js
@@ -231,7 +231,7 @@ async function retryCdn(code, url) {
     if (code === 'error') {
         let items = url.split("/")
         let fn = items[items.length-1]
-        let cndUrl = `https://github.com/yogayyy/Scripts/master/QuantumultX/Scripts/JD/jd-half-rain.json`
+        let cndUrl = `https://raw.githubusercontent.com/yogayyy/Scripts/master/QuantumultX/Scripts/JD/jd-half-rain.json`
         $.log(`召唤龙王失败, 召唤神龙: ${cndUrl}`)
         code = await redRainId(cndUrl)
     }
@@ -240,7 +240,7 @@ async function retryCdn(code, url) {
 }
 
 function rraUrl() {
-    let url = 'https://github.com/yogayyy/Scripts/master/QuantumultX/Scripts/JD/jd-half-rain.json'
+    let url = 'https://raw.githubusercontent.com/yogayyy/Scripts/master/QuantumultX/Scripts/JD/jd-half-rain.json'
     if($.isNode() && process.env.JD_HALF_RRA_URL){
         url = process.env.JD_HALF_RRA_URL
     }else if($.getdata('jdHalfRRAUrl')){
