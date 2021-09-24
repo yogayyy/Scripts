b23acc3b55c8a6c61d8bd8c3d362a0b7063d4ab2
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-09-24 14:58:39 +0800

    Update auto.js

--- QuantumultX/Scripts/auto.js
+++ QuantumultX/Scripts/auto.js
@@ -40,7 +40,7 @@ const $ = new Env(`前台自动阅读`);
     } else if (typeof $response !== "undefined") {
       // 如果重定向的是微信文章，改写重定向地址
       let url302 = ($response.headers && $response.headers['Location']) || ''
-      if (url302.match(/https?:\/\/szextshort.mp.weixin.qq.com\/s/)) {
+      if (url302.match(/https?:\/\/mp.weixin.qq.com\/s/)) {
         $response.headers['Location'] = $request.url.replace('/task/read', '/mock/read')
         $.done({headers: $response.headers})
       } else {
